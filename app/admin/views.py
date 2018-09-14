5df0ae1d9f5ba27f261ac3735d900cbb632cdf42
GESION\LuNan.Liu <lunan.liu@gesion.net>
2018-09-14 19:13:57 +0800

    no message

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,135 +1,75 @@
 # -*- coding: utf-8 -*-
+from . import home
+from flask import render_template, redirect, url_for
 
-from . import admin
-from flask import Flask, render_template, redirect, url_for
 
-
-@admin.route('/')
+@home.route('/')
 def index():
-    return render_template('admin/index.html')
+    return render_template("home/index.html")
 
 
 # 登录
-@admin.route('/login')
+@home.route('/login/')
 def login():
-    return render_template('admin/login.html')
+    return render_template("home/login.html")
 
 
 # 退出
-@admin.route('/logout')
+@home.route('/logout/')
 def logout():
-    return redirect(url_for('admin.login'))
+    return redirect(url_for("home.login"))
+
+
+# 注册
+@home.route('/register/')
+def register():
+    return render_template("home/register.html")
+
+
+# 会员中心
+@home.route('/user/')
+def user():
+    return render_template("home/user.html")
 
 
 # 修改密码
-@admin.route('/pwd')
+@home.route('/pwd/')
 def pwd():
-    return render_template('admin/pwd.html')
+    return render_template("home/pwd.html")
 
 
-# 添加标签
-@admin.route('/tag_add')
-def tag_add():
-    return render_template('admin/tag_add.html')
+# 评论记录
+@home.route('/comments/')
+def comments():
+    return render_template("home/comments.html")
 
 
-# 标签列表
-@admin.route('/tag_list')
-def tag_list():
-    return render_template('admin/tag_list.html')
+# 登录日志
+@home.route('/loginlog/')
+def loginlog():
+    return render_template("home/loginlog.html")
 
 
-# 添加电影
-@admin.route('/movie_add')
-def movie_add():
-    return render_template('admin/movie_add.html')
+# 电影收藏
+@home.route('/moviecol/')
+def moviecol():
+    return render_template("home/moviecol.html")
 
 
-# 电影列表
-@admin.route('/movie_list')
-def movie_list():
-    return render_template('admin/movie_list.html')
+# 主页动画
+@home.route('/animation/')
+def animation():
+    return render_template("home/animation.html")
 
 
-# 添加预告
-@admin.route('/preview_add')
-def preview_add():
-    return render_template('admin/preview_add.html')
+# 搜索
+@home.route('/search/')
+def search():
+    return render_template("home/search.html")
 
 
-# 预告列表
-@admin.route('/preview_list')
-def preview_list():
-    return render_template('admin/preview_list.html')
+# 播放
+@home.route('/play/')
+def play():
+    return render_template("home/play.html")
 
-
-# 会员列表
-@admin.route('/user_list')
-def user_list():
-    return render_template('admin/user_list.html')
-
-
-# 评论列表
-@admin.route('/comment_list')
-def comment_list():
-    return render_template('admin/comment_list.html')
-
-
-# 收藏列表
-@admin.route('/moviecol_list')
-def moviecol_list():
-    return render_template('admin/moviecol_list.html')
-
-
-# 操作日志列表
-@admin.route('/oplog_list')
-def oplog_list():
-    return render_template('admin/oplog_list.html')
-
-
-# 管理员登录日志列表
-@admin.route('/adminloginlog_list')
-def adminloginlog_list():
-    return render_template('admin/adminloginlog_list.html')
-
-
-# 会员登录日志列表
-@admin.route('/userloginlog_list')
-def userloginlog_list():
-    return render_template('admin/userloginlog_list.html')
-
-
-# 添加权限
-@admin.route('/auth_add')
-def auth_add():
-    return render_template('admin/auth_add.html')
-
-
-# 权限列表
-@admin.route('/auth_list')
-def auth_list():
-    return render_template('admin/auth_list.html')
-
-
-# 添加角色
-@admin.route('/role_add')
-def role_add():
-    return render_template('admin/role_add.html')
-
-
-# 角色列表
-@admin.route('/role_list')
-def role_list():
-    return render_template('admin/role_list.html')
-
-
-# 添加管理员
-@admin.route('/admin_add')
-def admin_add():
-    return render_template('admin/admin_add.html')
-
-
-# 管理员列表
-@admin.route('/admin_list')
-def admin_list():
-    return render_template('admin/admin_list.html')
